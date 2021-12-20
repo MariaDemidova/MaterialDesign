4120a585214c249f35755e2a09f7031c544f5bfb
MariaDemidova <demidova.ma@yandex.ru>
2021-12-20 21:04:35 +0500

    lesson1

---
+++ app/src/main/java/demidova/materialdesign/repository/PictureOfTheDayAPI.kt
@@ -0,0 +1,17 @@
+package demidova.materialdesign.repository
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PictureOfTheDayAPI {
+
+    @GET("planetary/apod")
+    fun getPictureOfTheDay(@Query("api_key") apiKey: String): Call<PictureOfTheDayResponseData>
+
+    @GET("planetary/apod")
+    fun getPotdByYesterdey(
+        @Query("api_key") apiKey: String,
+        @Query("date") date: String
+    ): Call<PictureOfTheDayResponseData>
+}
