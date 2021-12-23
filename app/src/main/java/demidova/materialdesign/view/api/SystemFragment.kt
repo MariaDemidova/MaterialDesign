8774fa87d3c120aa2982791d6bf192fee411a54f
MariaDemidova <demidova.ma@yandex.ru>
2021-12-23 13:47:49 +0500

    lesson3 start

---
+++ app/src/main/java/demidova/materialdesign/view/api/SystemFragment.kt
@@ -0,0 +1,27 @@
+package demidova.materialdesign.view.api
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import demidova.materialdesign.R
+
+
+
+
+class SystemFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_system, container, false)
+    }
+
+    companion object {
+
+    }
+}
